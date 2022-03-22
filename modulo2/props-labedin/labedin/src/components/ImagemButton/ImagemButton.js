4d7a255ea65dacc926c9ce0edef9faaa99e34d1b
matheus_mantini <engmatheusmantini@gmail.com>
2022-03-22 19:03:16 -0300

    Substitui os componentes atuais por Styled Componentes

--- modulo2/props-labedin/labedin/src/components/ImagemButton/ImagemButton.js
+++ modulo2/props-labedin/labedin/src/components/ImagemButton/ImagemButton.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import './ImagemButton.css'
+import styled from 'styled-components';
+
+const Container = styled.div `
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 50px;
+  width: 200px;
+  padding: 15px 30px;
+  margin: 10px auto;
+  cursor: pointer;
+  a{
+    text-decoration: none;
+    color: black;
+  :hover{
+    text-decoration: underline;
+  }
+  }
+`;
+
+const Imagem = styled.img `
+    width: 30px;
+    margin-right: 10px;
+`;
+
 
 function ImagemButton(props) {
     return (
-        <div className="image-button-container">
-            <img src={ props.imagem }/>
-            <p>{ props.texto }</p>
-        </div>
+        <Container>
+            <Imagem src={ props.imagem }/>
+            <a href={props.link} target="_blank"><p>{ props.texto }</p></a>
+        </Container>
 
     )
 }
